59bfd04a573542800d1c41b025db62fa0be1cb24
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-11-10 15:17:31 +0100

    latest

--- kevin/order/views.py
+++ kevin/order/views.py
@@ -34,7 +34,7 @@ class OrderView(ModelViewSet):
                     #city = metadata.get("city")
                 )
                     #await 
-                    order_obj.save()   
+                    #order_obj.save()   
                 return Response({"data": "received"}, status=status.HTTP_201_CREATED)
             case _:
                 print(data)
