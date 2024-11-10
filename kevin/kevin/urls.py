c9d51af33de14f0673e0ae7807d90c8a6c33a7d4
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-11-10 13:43:24 +0100

    latest

--- kevin/kevin/urls.py
+++ kevin/kevin/urls.py
@@ -5,9 +5,11 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from app_config.views import AppModelRetrieveViewSet
 from books.views  import BooksViewSet
+from order.views import OrderView
 router = SimpleRouter(trailing_slash=True)
 router.register(r"config", AppModelRetrieveViewSet)
 router.register(r"books", BooksViewSet)
+router.register("orders", OrderView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls))
